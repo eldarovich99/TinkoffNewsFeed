9d9962469388446d9c1cb958f0435a10f20fd919
eldarovich99 <32201422+eldarovich99@users.noreply.github.com>
2019-05-16 09:43:26 +0300

    excessive entities removed

--- app/src/main/java/com/eldarovich99/tinkoffnews/data/db/entity/News.kt
+++ app/src/main/java/com/eldarovich99/tinkoffnews/data/db/entity/News.kt
@@ -1,9 +1,12 @@
 package com.eldarovich99.tinkoffnews.data.db.entity
 
-import android.arch.persistence.room.Embedded
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
-import com.eldarovich99.tinkoffnews.data.db.entity.embedded.Payload
 
 @Entity
-data class News(@Embedded var payload: Payload, var resultCode: String, var trackingId: String)
+data class News(var  name:String,
+                 @PrimaryKey var id: String,
+                 var text: String,
+                 var publicationDate: String,
+                 var bankInfoTypeId: String
+)
--- app/src/main/java/com/eldarovich99/tinkoffnews/data/network/TinkoffService.kt
+++ app/src/main/java/com/eldarovich99/tinkoffnews/data/network/TinkoffService.kt
@@ -1,6 +1,8 @@
 package com.eldarovich99.tinkoffnews.data.network
 
 import com.eldarovich99.tinkoffnews.data.db.entity.News
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -8,7 +10,8 @@ import retrofit2.http.GET
 
 private const val BASE_URL = "https://api.tinkoff.ru/"
 
-private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(MoshiConverterFactory.create()).build()
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(MoshiConverterFactory.create(moshi)).build()
 
 interface TinkoffService{
     @GET("v1/news")
